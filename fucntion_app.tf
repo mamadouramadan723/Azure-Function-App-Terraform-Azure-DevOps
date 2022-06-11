5888b0d5bc1dad3566632b9d90f17ac940e18339
mamadou lamarana diallo <mamadoulamaranadiallo@student.emi.ac.ma>
2022-06-11 17:08:44 +0000

    Updated fucntion_app.tf

--- fucntion_app.tf
+++ fucntion_app.tf
@@ -3,33 +3,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "myrmdLabRG"
+  name     = "myrmdLab-RG"
   location = "eastus2"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "myrmdlabstorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
 resource "azurerm_service_plan" "example" {
-  name                = "myrmdlab-app-service-plan"
+  name                = "example"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  os_type             = "Windows"
-  sku_name            = "Y1"
-}
-
-resource "azurerm_windows_function_app" "example" {
-  name                = "myrmdlab-windows-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  storage_account_name = azurerm_storage_account.example.name
-  service_plan_id      = azurerm_service_plan.example.id
-
-  site_config {}
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
